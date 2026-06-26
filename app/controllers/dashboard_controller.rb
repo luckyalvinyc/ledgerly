634fe0d65dbdebed1cf1af07166fe9ebe786a3d8
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2026-06-26 17:31:52 +0800

    prompt to import on empty dashboard cards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,10 @@ class DashboardController < ApplicationController
 
   def index
     @period = Pnl::Period.month(Date.current)
+    counts = current_user.transactions.group(:bank_account_id).count
 
     @bank_accounts = current_user.bank_accounts.map do |bank_account|
-      [ bank_account, bank_account.profit_and_loss(@period) ]
+      [ bank_account, bank_account.profit_and_loss(@period), counts.fetch(bank_account.id, 0) ]
     end
   end
 end
