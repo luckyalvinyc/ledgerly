801d86a1f679a3fa557d59dca59997c501f9c196
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2026-06-26 11:01:18 +0800

    edit account name inline and delete account

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -4,13 +4,14 @@ class BankAccountsController < ApplicationController
   include Paginated
 
   before_action :redirect_if_unauthenticated
+  before_action :set_bank_account, only: [ :show, :edit, :update, :destroy ]
 
   def new
     @bank_account = current_user.bank_accounts.new
   end
 
   def create
-    @bank_account = current_user.bank_accounts.new(bank_account_params)
+    @bank_account = current_user.bank_accounts.new(bank_account_create_params)
     if @bank_account.save
       redirect_to @bank_account
     else
@@ -19,13 +20,37 @@ class BankAccountsController < ApplicationController
   end
 
   def show
-    @bank_account = current_user.bank_accounts.find(params[:id])
     @page = paginate(@bank_account.transactions.order(posted_on: :desc))
+  end
+
+  def edit
+  end
+
+  def update
+    if @bank_account.update(bank_account_update_params)
+      redirect_to @bank_account
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    name = @bank_account.name
+    @bank_account.destroy
+    redirect_to root_path, notice: t("flash.bank_accounts.deleted", name: name)
   end
 
   private
 
-    def bank_account_params
+    def set_bank_account
+      @bank_account = current_user.bank_accounts.find(params[:id])
+    end
+
+    def bank_account_create_params
       params.expect(bank_account: [ :name, :currency ])
+    end
+
+    def bank_account_update_params
+      params.expect(bank_account: [ :name ])
     end
 end
